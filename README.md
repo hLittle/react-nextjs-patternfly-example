0efcc0768fdb57e196cc176687a0d10172ccbdee
Heidi Little <hlittle@everquote.com>
2023-06-22 16:58:28 -0400

    Add prettier to eslint
    Add header and sidebar

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Libraries
+- React
+- Next.js
+- Patternfly
+- React Query
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,55 @@
-'use client';
+"use client";
 
-import '@patternfly/react-core/dist/styles/base.css';
-import { Button, Page } from "@patternfly/react-core";
+import "@patternfly/react-core/dist/styles/base.css";
+import {
+  Button,
+  Masthead,
+  MastheadMain,
+  MastheadToggle,
+  Page,
+  PageSidebar,
+  PageToggleButton,
+} from "@patternfly/react-core";
+import { BarsIcon, HomeIcon } from "@patternfly/react-icons";
+import { useState } from "react";
 
 export default function Home() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const onNavToggle = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const header = (
+    <Masthead>
+      <MastheadToggle>
+        <PageToggleButton
+          aria-label="Global navigation"
+          variant="plain"
+          isNavOpen={isNavOpen}
+          onNavToggle={onNavToggle}
+          id="vertical-nav-toggle"
+        >
+          <BarsIcon />
+        </PageToggleButton>
+      </MastheadToggle>
+      <MastheadMain>
+        <MastheadMain href="/">
+          <HomeIcon />
+        </MastheadMain>
+      </MastheadMain>
+    </Masthead>
+  );
+
+  const sidebar = (
+    <PageSidebar nav="Navigation" isNavOpen={isNavOpen} id="vertical-sidebar" />
+  );
+
   return (
-    <Page>
-      <Button type="button" variant="primary">Hello world</Button>
+    <Page header={header} sidebar={sidebar}>
+      <Button type="button" variant="primary">
+        Hello world
+      </Button>
     </Page>
-  )
+  );
 }
